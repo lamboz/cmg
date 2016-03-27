cc8fdcda74df56fdfe3927ff2e9cc8e524daa1b8
Pablo Cordero <pablocordero@gmail.com>
2016-03-27 12:06:05 -0600

    Add coming soon page

--- config.rb
+++ config.rb
@@ -17,8 +17,8 @@ page '/*.txt', layout: false
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
 # General configuration
-
 activate :i18n, :mount_at_root => :es
+activate :sprockets
 
 activate :relative_assets
 set :relative_links, true
@@ -53,4 +53,11 @@ after_configuration do
   @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
   sprockets.append_path File.join "#{root}", @bower_config["directory"]
   sprockets.append_path File.join "#{root}", @bower_config["directory"], 'foundation-sites/scss'
+  sprockets.append_path File.join "#{root}", @bower_config["directory"], 'jquery'
+  sprockets.append_path File.join "#{root}", @bower_config["directory"], 'font-awesome/scss'
 end
+
+
+
+#set :images_dir, 'fonts'
+#sprockets.import_asset('font-awesome/fonts/FontAwesome.otf') {|p| "#{images_dir}/FontAwesome.otf"}
